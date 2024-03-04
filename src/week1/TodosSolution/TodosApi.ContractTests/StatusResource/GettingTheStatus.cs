cc2410bde9fc3bcba0c381578bc182f34d327758
Joe Barkie <josephbarkie@hotmail.com>
2024-03-04 16:21:00 -0500

    Add unit tests
    Add new test project and unit tests to test the status of the API.

---
+++ src/week1/TodosSolution/TodosApi.ContractTests/StatusResource/GettingTheStatus.cs
@@ -0,0 +1,17 @@
+﻿using Alba;
+namespace TodosApi.ContractTests.StatusResource;
+
+public class GettingTheStatus
+{
+    [Fact]
+    public async Task ReturnsOk()
+    {
+        var host = await AlbaHost.For<Program>();
+
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/status");
+            api.StatusCodeShouldBeOk();
+        });
+    }
+}
---
+++ src/week1/TodosSolution/TodosApi.ContractTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+namespace TodosApi.ContractTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        // Given 
+        int a = 10, b = 20, answer;
+        // When
+        answer = a + b;
+        // Then
+        Assert.Equal(30, answer);
+    }
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(2, 2, 4)]
+    [InlineData(10, 2, 12)]
+    public void CanAddIntegers(int a, int b, int expected)
+    {
+        int answer = a + b;
+
+        Assert.Equal(expected, answer);
+    }
+}
