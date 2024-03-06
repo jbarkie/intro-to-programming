99f09e4aefec77ab8bbc98ed9b4a412c6688cc08
Joe Barkie <josephbarkie@hotmail.com>
2024-03-06 16:33:12 -0500

    Complete TDD Kata
    This code was written as part of a pair-programming TDD Kata exercise inspired from https://osherove.com/tdd-kata-1

---
+++ src/week1/StringCalculator - Wed Mar 6/StringCalculator.Tests/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace StringCalculator.Tests;
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        int result = 0;
+        if (string.IsNullOrEmpty(numbers)) return result;
+        string[] numbersStringArray = numbers.Split(',', '\n');
+        foreach (string number in numbersStringArray)
+        {
+            result += int.Parse(number);
+        }
+        return result;
+    }
+
+
+}
---
+++ src/week1/StringCalculator - Wed Mar 6/StringCalculator.Tests/Part1Tests.cs
@@ -0,0 +1,51 @@
+﻿// https://osherove.com/tdd-kata-1
+namespace StringCalculator.Tests;
+public class Part1Tests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("");
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void OneNumberDoesNotChange()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("1");
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void TwoNumbersAddedCorrectly()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("2,2");
+        Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void MoreThanTwoNumbersAddedCorrectly()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("1,2,3,4,5");
+        Assert.Equal(15, result);
+    }
+
+    [Fact]
+    public void NewlineDelimiterSupported()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("1\n2,3");
+        Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void IncorrectFormatThrowsException()
+    {
+        var calculator = new Calculator();
+        Assert.Throws<FormatException>(() => calculator.Add("1\n,"));
+    }
+}
