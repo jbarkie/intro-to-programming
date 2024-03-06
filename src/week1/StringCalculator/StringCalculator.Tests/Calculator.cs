3176504a14d7bd36dc077d160c06482ef6ba1aa6
Joe Barkie <josephbarkie@hotmail.com>
2024-03-06 16:32:20 -0500

    Add StringCalculator project
    This template project will be used for a pair-programming TDD Kata exercise.

---
+++ src/week1/StringCalculator/StringCalculator.Tests/Calculator.cs
@@ -0,0 +1,8 @@
+﻿namespace StringCalculator.Tests;
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        return -42;
+    }
+}
---
+++ src/week1/StringCalculator/StringCalculator.Tests/Part1Tests.cs
@@ -0,0 +1,12 @@
+﻿// https://osherove.com/tdd-kata-1
+namespace StringCalculator.Tests;
+public class Part1Tests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add("");
+        Assert.Equal(0, result);
+    }
+}
