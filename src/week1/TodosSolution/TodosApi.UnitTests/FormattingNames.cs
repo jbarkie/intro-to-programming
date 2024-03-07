aebad0429355137d80272de79ef0ce72e3e2da48
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 16:36:14 -0500

    Add unit tests
    Test that names are formatted correctly.

---
+++ src/week1/TodosSolution/TodosApi.UnitTests/FormattingNames.cs
@@ -0,0 +1,15 @@
+﻿namespace TodosApi.UnitTests;
+public class FormattingNames
+{
+    [Theory]
+    [InlineData("Bob", "Smith", "Bob Smith")]
+    [InlineData("Jill", "Jones", "Jill Jones")]
+    public void NamesAreFormattedProperly(string firstName, string lastName, string expected)
+    {
+        var utils = new Utils();
+
+        var fullName = utils.FormatName(firstName, lastName);
+
+        Assert.Equal(expected, fullName);
+    }
+}
