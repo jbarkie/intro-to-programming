55878a464acbf8839481a753bc63d54f711def71
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 16:56:45 -0500

    Complete TDD Kata Part 2
    Inspired from https://osherove.com/tdd-kata-2

---
+++ src/week1/StringCalculator -  Thurs Mar 7/StringCalculator.Tests/InteractionTests.cs
@@ -0,0 +1,31 @@
+﻿using NSubstitute;
+
+namespace StringCalculator.Tests;
+public class InteractionTests
+{
+    [Theory]
+    [InlineData("1,2", "3")]
+    [InlineData("42", "42")]
+    public void ResultIsLogged(string numbers, string expected)
+    {
+        var mockedLogger = Substitute.For<ILogger>();
+        var mockedWebService = Substitute.For<IWebService>();
+        var calculator = new Calculator(mockedLogger, mockedWebService);
+        calculator.Add(numbers);
+        mockedLogger.Received().Write(expected);
+        mockedWebService.DidNotReceive().NotifyOfLoggingError(Arg.Any<string>());
+    }
+
+    [Fact]
+    public void WhenTheLoggerCrashesYouCallAWebService()
+    {
+        var stubbedLogger = Substitute.For<ILogger>();
+        stubbedLogger.When(s => s.Write(Arg.Any<string>())).Throw<LoggerException>();
+
+        var mockedWebService = Substitute.For<IWebService>();
+        var calculator = new Calculator(stubbedLogger, mockedWebService);
+
+        calculator.Add("10");
+        mockedWebService.Received().NotifyOfLoggingError("Logger Failure 10");
+    }
+}
