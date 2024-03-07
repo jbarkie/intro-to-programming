9cc4be9c12d85489950b1d69b8c3de165cd9a5a3
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 13:13:51 -0500

    Test Gold account deposit bonuses
    Test whether a Gold account applies the appropriate deposit bonus. This uses the NSubstitute library to mock the creation of a stubbed out bonus calculator.

---
+++ src/week1/BankingSolution/Banking.UnitTests/Accounts/GoldCustomerDeposits.cs
@@ -0,0 +1,26 @@
+﻿using Bank;
+using NSubstitute;
+
+namespace Banking.UnitTests.Accounts;
+public class GoldCustomerDeposits
+{
+
+    [Fact]
+    public void MakingAGoldAccountDepositEarnsBonus()
+    {
+        // Given
+        var stubbedBonusCalculator = Substitute.For<ICalculateBonusesForDeposits>();
+        var account = new BankAccount(stubbedBonusCalculator);
+        var amountToDeposit = 142.23M;
+        var expectedBonus = 13.23M;
+        var openingBalance = account.GetBalance();
+        stubbedBonusCalculator.CalculateDepositBonusFor(openingBalance, amountToDeposit).Returns(expectedBonus);
+
+        // When
+        // WTCYWTH
+        account.Deposit(142.23M);
+
+        // Then
+        Assert.Equal(openingBalance + amountToDeposit + expectedBonus, account.GetBalance());
+    }
+}
