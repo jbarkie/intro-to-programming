0bc8040315bc30a61023ecbb620d7f824d4014f2
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 17:06:36 -0500

    Verify business hours
    Test that custom clock type validates whether the bank is open or closed.

---
+++ src/week1/BankingSolution/Banking.UnitTests/BusinessClock/StandardBusinessClockTests.cs
@@ -0,0 +1,23 @@
+﻿using Bank;
+using NSubstitute;
+
+namespace Banking.UnitTests.BusinessClock;
+public class StandardBusinessClockTests
+{
+    [Fact]
+    public void WeAreClosed()
+    {
+        var stubbedClock = Substitute.For<IClock>();
+        stubbedClock.GetCurrentLocalTime().Returns(new DateTime(2024, 3, 7, 17, 00, 00));
+        var clock = new StandardBusinessClock(stubbedClock);
+        Assert.False(clock.IsOpen());
+    }
+    [Fact]
+    public void WeAreOpen()
+    {
+        var stubbedClock = Substitute.For<IClock>();
+        stubbedClock.GetCurrentLocalTime().Returns(new DateTime(2024, 3, 7, 16, 59, 59));
+        var clock = new StandardBusinessClock(stubbedClock);
+        Assert.True(clock.IsOpen());
+    }
+}
