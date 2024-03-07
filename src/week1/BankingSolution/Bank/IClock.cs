f38c5d4e5be61a89e2d3daab138e9fa48f44bea8
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 17:01:25 -0500

    Define and implement interface
    The IClock interface defines a method to return the current local time.

---
+++ src/week1/BankingSolution/Bank/IClock.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Bank;
+
+public interface IClock
+{
+    DateTime GetCurrentLocalTime();
+}
+
+public class Clock : IClock
+{
+    public DateTime GetCurrentLocalTime()
+    {
+        return DateTime.Now;
+    }
+}
