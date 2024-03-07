1a9755353f0bb58c5483e31ae606a943f70af796
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 17:03:03 -0500

    Implement interface
    Bank is open if the current local time is between 9am and 5pm.

---
+++ src/week1/BankingSolution/Bank/StandardBusinessClock.cs
@@ -0,0 +1,9 @@
+﻿namespace Bank;
+public class StandardBusinessClock(IClock _clock) : IProvideTheBusinessClock
+{
+    public bool IsOpen()
+    {
+        DateTime now = _clock.GetCurrentLocalTime();
+        return (now.Hour >= 9 && now.Hour < 17) ? true : false;
+    }
+}
