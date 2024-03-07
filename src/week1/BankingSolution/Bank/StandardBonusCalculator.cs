a12bba6909affaf2bbb29147896044a0d14c60a5
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 13:08:33 -0500

    Implement interface
    This class implements the bonus calculator interface and defines the method for calculating a Gold account bonus.

---
+++ src/week1/BankingSolution/Bank/StandardBonusCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace Bank;
+public class StandardBonusCalculator : ICalculateBonusesForDeposits
+{
+    public decimal CalculateBonus(decimal balance, decimal amountToDeposit)
+    {
+        return balance >= 5000M ? amountToDeposit * .10M : 0;
+    }
+
+    public decimal CalculateDepositBonusFor(decimal currentBalance, decimal amountToDeposit)
+    {
+        return CalculateBonus(currentBalance, amountToDeposit);
+    }
+}
