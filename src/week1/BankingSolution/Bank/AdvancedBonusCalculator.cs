4e3ef850989d0581d6514a481b2f0b7563153bc5
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 17:04:03 -0500

    Restrict deposit bonus
    Gold account deposit bonus should only apply during business hours.

---
+++ src/week1/BankingSolution/Bank/AdvancedBonusCalculator.cs
@@ -0,0 +1,18 @@
+﻿namespace Bank;
+public class AdvancedBonusCalculator(IProvideTheBusinessClock clock) : ICalculateBonusesForDeposits
+{
+    private readonly IProvideTheBusinessClock _clock = clock;
+
+    public decimal CalculateDepositBonusFor(decimal currentBalance, decimal amountToDeposit)
+    {
+        // if between 9am-5pm M-F not on a holiday
+        // then, if currentBalance >= 5000, return bonus of 10%
+        // otherwise, return no bonus
+        // WTCYWYH
+        if (_clock.IsOpen() && currentBalance >= 5000M)
+        {
+            return amountToDeposit * .10M;
+        }
+        return 0;
+    }
+}
