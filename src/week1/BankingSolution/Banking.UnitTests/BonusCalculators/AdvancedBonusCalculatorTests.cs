28ef13bd1dbfae236a8f007381d8e1fd679ac94b
Joe Barkie <josephbarkie@hotmail.com>
2024-03-07 17:04:44 -0500

    Verify deposit bonus restrictions
    Verify that deposit bonuses apply only during business hours and for the appropriate account balance.

---
+++ src/week1/BankingSolution/Banking.UnitTests/BonusCalculators/AdvancedBonusCalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Bank;
+using NSubstitute;
+
+namespace Banking.UnitTests.BonusCalculators;
+public class AdvancedBonusCalculatorTests
+{
+    [Fact]
+    public void DuringBusinessHoursWithAdequateBalance()
+    {
+        var stubbedClock = Substitute.For<IProvideTheBusinessClock>();
+        stubbedClock.IsOpen().Returns(true);
+        var calculator = new AdvancedBonusCalculator(stubbedClock);
+        var bonus = calculator.CalculateDepositBonusFor(5000M, 100);
+        Assert.Equal(10, bonus);
+    }
+
+    [Fact]
+    public void DuringBusinessHoursWithInadequateBalance()
+    {
+        var stubbedClock = Substitute.For<IProvideTheBusinessClock>();
+        stubbedClock.IsOpen().Returns(true);
+        var calculator = new AdvancedBonusCalculator(stubbedClock);
+        var bonus = calculator.CalculateDepositBonusFor(4999.99M, 100);
+        Assert.Equal(0, bonus);
+    }
+
+    [Fact]
+    public void OutsideBusinessHours()
+    {
+        var stubbedClock = Substitute.For<IProvideTheBusinessClock>();
+        stubbedClock.IsOpen().Returns(false);
+        var calculator = new AdvancedBonusCalculator(stubbedClock);
+        var bonus = calculator.CalculateDepositBonusFor(10000M, 100);
+        Assert.Equal(0, bonus);
+    }
+}
