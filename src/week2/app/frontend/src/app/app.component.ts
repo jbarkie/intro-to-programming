6eddc69d9b45a00b0457eb2b6fe11443c538f684
Joe Barkie <josephbarkie@hotmail.com>
2024-03-11 16:36:05 -0400

    Learn routing
    Learn routing in Angular using the RouterOutlet.

--- src/week2/app/frontend/src/app/app.component.ts
+++ src/week2/app/frontend/src/app/app.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
-import { PageHeaderComponent } from "./components/page-header/page-header.component";
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    template: `
-    <app-page-header />
-    <main>
-      <p>Our stuff goes here</p>
-    </main>
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <div class="container mx-auto">
+      <app-page-header />
+      <nav>
+        <app-nav-bar />
+      </nav>
+      <main>
+        <router-outlet />
+      </main>
+    </div>
   `,
-    styles: [],
-    imports: [PageHeaderComponent]
+  styles: [],
+  imports: [PageHeaderComponent, RouterOutlet, NavBarComponent],
 })
-export class AppComponent {
-}
+export class AppComponent {}
