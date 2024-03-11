2695c9a7a2109a5935a441c651ef3d99f2907fb4
Joe Barkie <josephbarkie@hotmail.com>
2024-03-11 16:36:21 -0400

    Add demo component

---
+++ src/week2/app/frontend/src/app/components/demo/demo.component.ts
@@ -0,0 +1,20 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      {{ counter() }}
+    </p>
+    <button (click)="doIt()" class="btn btn-primary">Increment</button>
+  `,
+  styles: ``,
+})
+export class DemoComponent {
+  counter = signal(0);
+  doIt() {
+    this.counter.set(this.counter() + 1);
+  }
+}
