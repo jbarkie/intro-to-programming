1acd5233c28aa09bca54e449b99c78a65ce33845
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 15:38:13 -0400

    Add settings button
    Link to prefs components.

--- src/week2/app/frontend/src/app/components/page-header/page-header.component.ts
+++ src/week2/app/frontend/src/app/components/page-header/page-header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   template: `
     <header>
       <h1 class="font-black text-3xl">Our Angular Application</h1>
       <p>Just Learning Here</p>
+      <a routerLink="/prefs">⚙️</a>
     </header>
   `,
   styles: ``,
