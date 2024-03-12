b001db0d8c72fa57124dab7ce4c079cb1bf8762b
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 16:58:44 -0400

    Update reducers
    To reflect new changes in actions.

--- src/week2/app/frontend/src/app/pages/counter/state/index.ts
+++ src/week2/app/frontend/src/app/pages/counter/state/index.ts
@@ -1,12 +1,13 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
-import { CountByValues, CounterAction } from './actions';
+import { CountByValues, CounterActions } from './actions';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 export interface CounterState {
   current: number;
   by: CountByValues;
 }
 
-const initialState: CounterState = {
+export const initialState: CounterState = {
   current: 0,
   by: 1,
 };
@@ -15,18 +16,22 @@ export const counterFeature = createFeature({
   name: 'counter',
   reducer: createReducer(
     initialState,
-    on(CounterAction.incremented, (state) => ({
+    on(CounterActions.incremented, (state) => ({
       ...state,
       current: state.current + state.by,
     })),
-    on(CounterAction.decremented, (state) => ({
+    on(CounterActions.decremented, (state) => ({
       ...state,
       current: state.current - state.by,
     })),
-    on(CounterAction.reset, () => initialState),
-    on(CounterAction.countByChanged, (state, action) => ({
+    on(CounterActions.reset, (state) => ({
+      ...state,
+      current: initialState.current,
+    })),
+    on(CounterActions.countByChanged, (state, action) => ({
       ...state,
       by: action.payload,
-    }))
+    })),
+    on(CounterActions.state, (_, action) => action.payload)
   ),
 });
