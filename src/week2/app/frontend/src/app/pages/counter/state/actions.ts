335494cce86787a63e1c7ee0a86563bf74d87b3a
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 15:38:54 -0400

    Dispatch action and update state
    Set increment/decrement interval.

--- src/week2/app/frontend/src/app/pages/counter/state/actions.ts
+++ src/week2/app/frontend/src/app/pages/counter/state/actions.ts
@@ -1,10 +1,12 @@
-import { createActionGroup, emptyProps } from '@ngrx/store';
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
 
+export type CountByValues = 1 | 3 | 5;
 export const CounterAction = createActionGroup({
   source: 'Counter Component Events',
   events: {
     Incremented: emptyProps(),
     Decremented: emptyProps(),
     Reset: emptyProps(),
+    'Count By Changed': props<{ payload: CountByValues }>(),
   },
 });
--- src/week2/app/frontend/src/app/pages/counter/state/index.ts
+++ src/week2/app/frontend/src/app/pages/counter/state/index.ts
@@ -1,20 +1,32 @@
 import { createFeature, createReducer, on } from '@ngrx/store';
-import { CounterAction } from './actions';
+import { CountByValues, CounterAction } from './actions';
 
 export interface CounterState {
   current: number;
+  by: CountByValues;
 }
 
 const initialState: CounterState = {
   current: 0,
+  by: 1,
 };
 
 export const counterFeature = createFeature({
   name: 'counter',
   reducer: createReducer(
     initialState,
-    on(CounterAction.incremented, (state) => ({ current: state.current + 1 })),
-    on(CounterAction.decremented, (state) => ({ current: state.current - 1 })),
-    on(CounterAction.reset, (state) => initialState)
+    on(CounterAction.incremented, (state) => ({
+      ...state,
+      current: state.current + state.by,
+    })),
+    on(CounterAction.decremented, (state) => ({
+      ...state,
+      current: state.current - state.by,
+    })),
+    on(CounterAction.reset, () => initialState),
+    on(CounterAction.countByChanged, (state, action) => ({
+      ...state,
+      by: action.payload,
+    }))
   ),
 });
