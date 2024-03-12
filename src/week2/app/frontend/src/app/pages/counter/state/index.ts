7e8450fad08c75c45902a641495e43d8ba9999c1
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 14:39:52 -0400

    Add counter state and feature

---
+++ src/week2/app/frontend/src/app/pages/counter/state/index.ts
@@ -0,0 +1,19 @@
+import { createFeature, createReducer, on } from '@ngrx/store';
+import { CounterAction } from './actions';
+
+export interface CounterState {
+  current: number;
+}
+
+const initialState: CounterState = {
+  current: 0,
+};
+
+export const counterFeature = createFeature({
+  name: 'counter',
+  reducer: createReducer(
+    initialState,
+    on(CounterAction.incremented, (state) => ({ current: state.current + 1 })),
+    on(CounterAction.decremented, (state) => ({ current: state.current - 1 }))
+  ),
+});
