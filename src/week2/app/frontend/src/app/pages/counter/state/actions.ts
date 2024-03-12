f677464300ba958926582d3f8258f210effc8b3c
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 16:57:54 -0400

    Update actions
    Add new action when counter is entered by user. Add state.

--- src/week2/app/frontend/src/app/pages/counter/state/actions.ts
+++ src/week2/app/frontend/src/app/pages/counter/state/actions.ts
@@ -1,12 +1,15 @@
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { CounterState } from '.';
 
 export type CountByValues = 1 | 3 | 5;
-export const CounterAction = createActionGroup({
+export const CounterActions = createActionGroup({
   source: 'Counter Component Events',
   events: {
     Incremented: emptyProps(),
     Decremented: emptyProps(),
     Reset: emptyProps(),
     'Count By Changed': props<{ payload: CountByValues }>(),
+    'Counter Entered': emptyProps(),
+    State: props<{ payload: CounterState }>(),
   },
 });
