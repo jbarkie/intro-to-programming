a967af9bafa02e3f92eb50237eb69febc100245a
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 15:37:46 -0400

    Update routes
    Add prefs component.

--- src/week2/app/frontend/src/app/app.routes.ts
+++ src/week2/app/frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
 import { CounterComponent } from './pages/counter/counter.component';
+import { PrefsComponent } from './pages/prefs/prefs.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'counter',
     component: CounterComponent,
+  },
+  {
+    path: 'prefs',
+    component: PrefsComponent,
   },
   {
     path: '**',
