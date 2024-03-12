b1c82a62b36f0787bc17fa9a602c653371bb8860
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 16:56:28 -0400

    Dispatch action
    Upon app start.

--- src/week2/app/frontend/src/app/app.component.ts
+++ src/week2/app/frontend/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ApplicationActions } from './actions';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
@@ -20,4 +22,8 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
   styles: [],
   imports: [PageHeaderComponent, RouterOutlet, NavBarComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(store: Store) {
+    store.dispatch(ApplicationActions.applicationStarted());
+  }
+}
