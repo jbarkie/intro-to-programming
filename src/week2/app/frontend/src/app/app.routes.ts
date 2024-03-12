73e5197e0d10ce6632bd068c8e5b50fa75748dc9
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 12:06:47 -0400

    Add counter to routes

--- src/week2/app/frontend/src/app/app.routes.ts
+++ src/week2/app/frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { TodoListComponent } from './pages/todo-list/todo-list.component';
+import { CounterComponent } from './pages/counter/counter.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'todo-list',
     component: TodoListComponent,
+  },
+  {
+    path: 'counter',
+    component: CounterComponent,
   },
   {
     path: '**',
