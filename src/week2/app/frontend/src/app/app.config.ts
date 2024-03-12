d9e6f6ae69f6e6dca73b46d0820493be9cfff80e
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 14:40:59 -0400

    Provide initial counter state to application

--- src/week2/app/frontend/src/app/app.config.ts
+++ src/week2/app/frontend/src/app/app.config.ts
@@ -2,13 +2,15 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { reducers } from './state';
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { counterFeature } from './pages/counter/state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(reducers),
+    provideState(counterFeature),
     provideStoreDevtools(),
   ],
 };
