6d91b790920d682691f83ac76c5e880332d4b0cc
Joe Barkie <josephbarkie@hotmail.com>
2024-03-12 14:41:16 -0400

    Define actions

---
+++ src/week2/app/frontend/src/app/pages/counter/state/actions.ts
@@ -0,0 +1,9 @@
+import { createActionGroup, emptyProps } from '@ngrx/store';
+
+export const CounterAction = createActionGroup({
+  source: 'Counter Component Events',
+  events: {
+    Incremented: emptyProps(),
+    Decremented: emptyProps(),
+  },
+});
