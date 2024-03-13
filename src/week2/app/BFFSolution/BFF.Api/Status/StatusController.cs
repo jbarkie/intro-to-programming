477abfa276de15d3d74f5afd333b28430af1e9b3
Joe Barkie <josephbarkie@hotmail.com>
2024-03-13 11:20:48 -0400

    Add new ASP.NET Core web API
    New API to be controller-based.

---
+++ src/week2/app/BFFSolution/BFF.Api/Status/StatusController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BFF.Api.Status;
+
+public class StatusController : ControllerBase
+{
+    // GET /status
+    [HttpGet("/status")]
+    public IActionResult GetTheStatus()
+    {
+        return Ok("Everything is groovy");
+    }
+}
