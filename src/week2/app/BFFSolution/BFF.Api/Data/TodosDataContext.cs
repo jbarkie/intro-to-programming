6a1328f3c3b1b54a8f21db7544c02cb7a43a8705
Joe Barkie <josephbarkie@hotmail.com>
2024-03-13 16:57:48 -0400

    Create entity and migration
    Connect to database.

---
+++ src/week2/app/BFFSolution/BFF.Api/Data/TodosDataContext.cs
@@ -0,0 +1,20 @@
+﻿using BFF.Api.Todos;
+using Microsoft.EntityFrameworkCore;
+
+namespace BFF.Api.Data;
+
+public class TodosDataContext : DbContext
+{
+    public TodosDataContext(DbContextOptions<TodosDataContext> options) : base(options) { }
+
+    public DbSet<TodoEntity> Todos { get; set; }
+}
+
+public class TodoEntity
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public DateTimeOffset? DueDate { get; set; }
+    public TodoPriority? Priority { get; set; } // nullable
+    public DateTimeOffset? CreatedDate { get; set; }
+}
