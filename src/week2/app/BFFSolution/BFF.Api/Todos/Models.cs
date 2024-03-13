6341520501a889f1114f3e9039b1e82e9a77eb05
Joe Barkie <josephbarkie@hotmail.com>
2024-03-13 12:38:32 -0400

    Add new POST todos endpoint
    Create a new Todos object and return appropriate response to the client upon receiving a CreateTodosRequest object.

---
+++ src/week2/app/BFFSolution/BFF.Api/Todos/Models.cs
@@ -0,0 +1,18 @@
+﻿namespace BFF.Api.Todos;
+
+public record CreateTodoRequest
+{
+    public string Description { get; set; } = string.Empty;
+    public DateTimeOffset? DueDate { get; set; }
+    public TodoPriority? Priority { get; set; } // nullable
+}
+
+public enum TodoPriority { Low, High };
+
+public record CreateTodoResponse
+{
+    public Guid Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public DateTimeOffset? DueDate { get; set; }
+    public TodoPriority? Priority { get; set; } // nullable
+}
---
+++ src/week2/app/BFFSolution/BFF.Api/Todos/TodosController.cs
@@ -0,0 +1,20 @@
+﻿using BFF.Api.Todos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bff.Api.Todos;
+
+public class TodosController : ControllerBase
+{
+    [HttpPost("/todos")]
+    public IActionResult AddATodo([FromBody] CreateTodoRequest request)
+    {
+        var response = new CreateTodoResponse
+        {
+            Id = Guid.NewGuid(),
+            Description = request.Description,
+            DueDate = request.DueDate,
+            Priority = request.Priority
+        };
+        return Ok(response);
+    }
+}
