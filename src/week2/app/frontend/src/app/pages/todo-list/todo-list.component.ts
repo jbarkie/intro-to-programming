ce1d8c9283510c6886a2ae701346ed7d15fe2c52
Joe Barkie <josephbarkie@hotmail.com>
2024-03-14 15:15:16 -0400

    Handle loading states
    Use a loading indicator and provide a means of refreshing the Todos list.

--- src/week2/app/frontend/src/app/pages/todo-list/todo-list.component.ts
+++ src/week2/app/frontend/src/app/pages/todo-list/todo-list.component.ts
@@ -4,6 +4,8 @@ import { TodoItemListComponent } from './components/todo-item-list/todo-item-lis
 import { TodoListItem } from './models';
 import { todosFeature } from './state';
 import { Store } from '@ngrx/store';
+import { TodoEffects } from './state/todos-effects';
+import { TodoCommands } from './state/actions';
 
 @Component({
   selector: 'app-todo-list',
@@ -11,22 +13,28 @@ import { Store } from '@ngrx/store';
   template: `
     <section>
       <h2 class="text-2xl font-bold">Your Todo List</h2>
+      <button (click)="reload()" class="btn btn-sm btn-primary">Refresh</button>
     </section>
+    @if(loaded()) {
     <div>
       <app-todo-entry (itemAdded)="addItem($event)" />
     </div>
     <div>
       <app-todo-item-list [list]="todoList()" />
     </div>
+    } @else {
+    <span class="loading loading-ring loading-lg"></span>
+    }
   `,
   styles: ``,
   imports: [TodoEntryComponent, TodoItemListComponent],
 })
 export class TodoListComponent {
   constructor(private store: Store) {}
-
+  loaded = this.store.selectSignal(todosFeature.selectIsLoaded);
   todoList = this.store.selectSignal(todosFeature.selectTodoListItems);
-  addItem(item: { description: string }) {
-    // TODO : dispatch action
+  addItem(item: { description: string }) {}
+  reload() {
+    this.store.dispatch(TodoCommands.loadTodos());
   }
 }
