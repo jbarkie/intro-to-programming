903e44b7396ac9769117926dfb9f7e539f650d96
Joe Barkie <josephbarkie@hotmail.com>
2024-03-14 13:48:42 -0400

    Add actions

---
+++ src/week2/app/frontend/src/app/pages/todo-list/state/actions.ts
@@ -0,0 +1,24 @@
+import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { TodoEntity } from '../types';
+
+// Command - order ("do this thing") - usually expects something specific to happen as a result
+export const TodoCommands = createActionGroup({
+  source: 'Todo Commands',
+  events: {
+    'Load Todos': emptyProps(),
+  },
+});
+
+// Event - something that happened - that might mean multiple things need to happen, but the event is decoupled from that
+export const TodoEvents = createActionGroup({
+  source: 'Todo Events',
+  events: {},
+});
+
+// Document - often the result of a command
+export const TodoDocuments = createActionGroup({
+  source: 'Todo Documents',
+  events: {
+    todos: props<{ payload: TodoEntity[] }>(),
+  },
+});
