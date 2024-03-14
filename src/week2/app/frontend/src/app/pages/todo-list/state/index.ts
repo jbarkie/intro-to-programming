584e7841b0ecf53d0c4e13b0a934d33ec2539c08
Joe Barkie <josephbarkie@hotmail.com>
2024-03-14 15:17:17 -0400

    Update state

--- src/week2/app/frontend/src/app/pages/todo-list/state/index.ts
+++ src/week2/app/frontend/src/app/pages/todo-list/state/index.ts
@@ -10,15 +10,21 @@ import { TodoEntity } from '../types';
 import { TodoListItem } from '../models';
 import { TodoDocuments } from './actions';
 
-export interface TodosState extends EntityState<TodoEntity> {}
+export interface TodosState extends EntityState<TodoEntity> {
+  isLoaded: boolean;
+}
 
 const adapter = createEntityAdapter<TodoEntity>();
-const initialState: TodosState = adapter.getInitialState();
+const initialState: TodosState = adapter.getInitialState({
+  isLoaded: false,
+});
 export const todosFeature = createFeature({
   name: 'todos',
   reducer: createReducer(
     initialState,
-    on(TodoDocuments.todos, (s, a) => adapter.setAll(a.payload, s))
+    on(TodoDocuments.todos, (s, a) => adapter.setAll(a.payload, s)),
+    on(TodoDocuments.todo, (s, a) => adapter.addOne(a.payload, s)),
+    on(TodoDocuments.todos, (s) => ({ ...s, isLoaded: true }))
   ),
   extraSelectors: ({ selectTodosState }) => {
     const { selectAll } = adapter.getSelectors();
